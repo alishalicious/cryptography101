f82a3ffbc9efa996dcac99c32d92e0aff29530a2
iamhoor <hoorulain276@gmail.com>
2024-08-27 18:07:22 +0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,6 +18,15 @@ const config: DocsThemeConfig = {
   footer: {
     text: '© 2024 Cryptography101',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Cryptography101" />
+      <meta property="og:description" content="Your ultimate handbook for cryptography and stuff" />
+      <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
+      <link rel="icon" type="image/x-icon" sizes="any" href="/favicon.ico" />
+    </>
+  ),
 }
 
 export default config
