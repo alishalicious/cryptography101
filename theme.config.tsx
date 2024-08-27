fc864e6b06c21a830bdf2a166d5954549b9308d3
iamhoor <hoorulain276@gmail.com>
2024-08-27 17:27:35 +0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Cryptography101</span>,
   project: {
-    link: 'https://github.com/iamhoor/cryptography101.git',
+    link: 'https://github.com/iamhoor/cryptography101',
   },
   chat: {
     link: 'https://discord.gg/DpXy8CpN4U',
   },
-  docsRepositoryBase: 'https://github.com/iamhoor/cryptography101',
+  docsRepositoryBase: 'https://github.com/iamhoor/cryptography101/tree/main',
   footer: {
     text: '© 2024 Cryptography101',
   },
