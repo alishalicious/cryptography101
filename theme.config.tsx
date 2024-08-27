196526cdda59ec3c7fe074b580f78b0f770daf08
iamhoor <hoorulain276@gmail.com>
2024-08-27 18:42:39 +0500

    new

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,31 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  useNextSeoProps(){
+    return{
+      titleTemplate: '%s | Cryptography101- your ultimate handbook for cyrptography and stuff'
+    }
+  },
+  logo: <span>Cryptography101</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/iamhoor/cryptography101',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/DpXy8CpN4U',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/iamhoor/cryptography101/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2024 Cryptography101',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Cryptography101" />
+      <meta property="og:description" content="Your ultimate handbook for cryptography and stuff" />
+      <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
+      <link rel="icon" type="image/x-icon" sizes="any" href="/favicon.ico" />
+    </>
+  ),
 }
 
 export default config
