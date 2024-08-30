7e999f6b79533c31bb7a588277948f8ef70f8191
iamhoor <hoorulain276@gmail.com>
2024-08-30 21:22:38 +0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -21,7 +21,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Cryptography101" />
+      <meta property="og:title" content="Cryptography 101" />
       <meta property="og:description" content="Your ultimate handbook for cryptography and stuff" />
       <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
       <link rel="icon" type="image/x-icon" sizes="any" href="/favicon.ico" />
